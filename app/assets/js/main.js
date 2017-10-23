48ead29931014ef0975322832926b0bcc53cdc64
Mateus Paulino <mateus-web@live.com>
2017-10-23 12:04:52 -0200

    selecting elements

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,7 +1,10 @@
 import { Lightsaber } from './modulos/Lightsaber.js';
 
+let $ = document.querySelector.bind(document);
+
 let lightSaber = new Lightsaber();
+// lightSaber.elemimg.src = "./img/lightsaber-on.png";
 
 // console.log("oi");
-console.log(lightSaber.state);
+console.log(lightSaber.elemimg);
 
--- app/assets/js/modulos/Lightsaber.js
+++ app/assets/js/modulos/Lightsaber.js
@@ -1,9 +1,18 @@
 export class Lightsaber {
     constructor(){
+        let $ = document.querySelector.bind(document);
         this._on = false;
+        this._elem = $('.lightsaber');
+        this._elemimg = $('.lightsaber img');
     }
     get state(){
         return this._on;
+    }  
+    get elem(){
+        return this._elem;
+    } 
+    get elemimg(){
+        return this._elemimg;
     }    
 }
 
