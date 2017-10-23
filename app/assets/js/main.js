29871a337d05de2cc045fadaadb064cc02815a1c
Mateus Paulino <mateus-web@live.com>
2017-10-23 11:30:16 -0200

    Creating class

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,2 +1,7 @@
-// import { Defaults, Paths } from '../../../config';
+import { Lightsaber } from './modulos/Lightsaber.js';
+
+let lightSaber = new Lightsaber();
+
+// console.log("oi");
+console.log(lightSaber.state);
 
--- app/assets/js/modulos/Lightsaber.js
+++ app/assets/js/modulos/Lightsaber.js
@@ -1,8 +1,10 @@
-class Lightsaber {
+export class Lightsaber {
     constructor(){
-        this._on = true;
+        this._on = false;
     }
     get state(){
         return this._on;
     }    
 }
+
+// export { Lightsaber as default}
