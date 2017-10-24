085c54283b2321d719bbc03507fc53ffb97a8209
Mateus Paulino <mateus-web@live.com>
2017-10-24 12:09:52 -0200

    fixing audio manipulation on mobile

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,14 +1,18 @@
 import { Lightsaber } from './modulos/Lightsaber.js';
-import { Audio } from './modulos/Audio.js';
+import { AudioEffect } from './modulos/AudioEffect.js';
 
 let $ = document.querySelector.bind(document);
 
 let lightSaber = new Lightsaber();
 let lightSaberElem = lightSaber.elem;
 
-let audio = new Audio();
+let audio = new AudioEffect();
+
+let createdPlayerRight = null;
+let createdPlayerLeft = null;
 
 lightSaberElem.addEventListener('click', function(){
+
     if(lightSaber.state){
         lightSaber.elemimg.src = lightSaber.pathOff;
         lightSaber.setState(false);
@@ -18,7 +22,28 @@ lightSaberElem.addEventListener('click', function(){
         lightSaber.setState(true);        
         audio.on.play();
     }
+    
+    //teste
+    createdPlayerRight = new Audio('../audio/right.mp3');
+    createdPlayerRight.muted = true;
+
+    createdPlayerLeft = new Audio('../audio/left.mp3');
+    createdPlayerLeft.muted = true;
+
+    createdPlayerRight.play();
+    createdPlayerRight.play();
+
+    createdPlayerLeft.muted = false;
+    createdPlayerRight.muted = false;
+
+    // lightSaber.setState(true);
+
+    // audio.player.src = '../audio/on.mp3';
+    // audio.player.play();
+
 });
+
+
 
 let logMovimento = $('header h1');
 logMovimento.innerHTML = "parado";
@@ -27,15 +52,26 @@ if(window.DeviceOrientationEvent){
     let sinal = 0;
     window.addEventListener('deviceorientation',function(event){ 
         let movementGamma = event.gamma;
-        console.log(movementGamma);
+        // console.log(movementGamma);
         if(lightSaber.state){
             if(movementGamma>25){
                 logMovimento.innerHTML = "direita";
-                audio.right.play();
+
+                // audio.player.src = '../audio/right.mp3';
+                // audio.player.play();
+
+                createdPlayerRight.play();
+
+                // audio.right.play();
                 // playAudio("right");
             }else if(movementGamma<-25){
-                audio.left.play();
+                // audio.left.play();
                 logMovimento.innerHTML = "esquerda";
+
+                createdPlayerLeft.play();
+
+                // audio.player.src = '../audio/left.mp3';
+                // audio.player.play();
                 // playAudio("left");
             }else{
                 logMovimento.innerHTML = "parado";
--- app/assets/js/modulos/AudioEffect.js
+++ app/assets/js/modulos/AudioEffect.js
@@ -1,10 +1,14 @@
-export class Audio {
+export class AudioEffect {
     constructor(){
         let $ = document.querySelector.bind(document);
+        this._player = $('.audio#player');
         this._on = $('.audio#on');
         this._off = $('.audio#off');
         this._right = $('.audio#right');
         this._left = $('.audio#left');
+    }
+    get player(){
+        return this._player;
     }
     get on(){
         return this._on;
